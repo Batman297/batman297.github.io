959d6a8c0658cd82442ee066164caaef91216b4e
Tri Denda <tridenda.nk@gmail.com>
2023-04-27 13:21:15 +0700

    feat: installed montserrat font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from "@src/components/layout/layout.component";
-import "@src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+import "@src/styles/globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main className={montserrat.className}>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 }
