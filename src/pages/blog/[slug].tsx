1ea094021e1ad6231b335ead9a932d9b5b74460e
Tri Denda <tridenda.nk@gmail.com>
2023-04-28 11:28:37 +0700

    refactor: join clsx and date formater

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,4 +1,4 @@
-import dateFormatter from "@src/lib/helper";
+import { dateFormatter } from "@src/lib/helper";
 import fs from "fs";
 import matter from "gray-matter";
 import Markdown from "markdown-to-jsx";
