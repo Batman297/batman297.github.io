8908b380b2fc8f95b04f07b9df9cb9db3ad0a187
Tri Denda <tridenda.nk@gmail.com>
2023-04-29 00:23:53 +0700

    feat: add about page

--- data/about.md
+++ data/about.md
@@ -1,4 +1,8 @@
-## About me
+---
+title: "About Me"
+subtitle: "An obsessed guy who loves solving problems with science and technologies"
+date: "2023-04-23"
+---
 
 Tri is an obsessed web developer who loves solving problems and making meaningful impacts on others' lives through the products that he built. For him, creating products is about identifying problems to create effective solutions to solve the problems.
 
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,50 @@
-const About = () => {
-  return <h1>About</h1>;
+import fs from "fs";
+import matter from "gray-matter";
+import Markdown from "markdown-to-jsx";
+import path from "path";
+import { FC } from "react";
+
+// export const getStaticPaths = async () => {
+//   return {
+//     paths: [{ params: { slug: "about" } }],
+//     fallback: false, // can also be true or 'blocking'
+//   };
+// };
+
+export const getStaticProps = async () => {
+  const content = fs.readFileSync(path.join("data", "about.md"), "utf-8");
+  const matterResult = matter(content);
+
+  return {
+    props: {
+      about: {
+        title: matterResult.data.title,
+        subtitle: matterResult.data.subtitle,
+        date: matterResult.data.date,
+        content: matterResult.content,
+      },
+    },
+  };
+};
+
+type AboutProps = {
+  about: {
+    title: string;
+    subtitle: string;
+    date: string;
+    content: string;
+  };
+};
+
+const About: FC<AboutProps> = ({ about }) => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-gray-700 mt-12">{about.title}</h1>
+      <article className="prose">
+        <Markdown>{about.content}</Markdown>
+      </article>
+    </>
+  );
 };
 
 export default About;
